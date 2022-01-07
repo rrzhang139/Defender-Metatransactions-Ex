4b2d1143fb02df2ed5333a33fd399b92da751150
Richard Zhang <rrzhang@usc.edu>
2022-01-07 09:12:36 -0800

    added all built files

--- 01-defender-meta-txs/autotasks/relay/index.js
+++ 01-defender-meta-txs/autotasks/relay/index.js
@@ -13,7 +13,7 @@ async function relay(forwarder, request, signature, whitelist) {
   // Validate request on the forwarder contract
   const valid = await forwarder.verify(request, signature);
   if (!valid) throw new Error(`Invalid request`);
-  
+
   // Send meta-tx through relayer to the forwarder contract
   const gasLimit = (parseInt(request.gas) + 50000).toString();
   return await forwarder.execute(request, signature, { gasLimit });
@@ -24,13 +24,16 @@ async function handler(event) {
   if (!event.request || !event.request.body) throw new Error(`Missing payload`);
   const { request, signature } = event.request.body;
   console.log(`Relaying`, request);
-  
+
   // Initialize Relayer provider and signer, and forwarder contract
-  const credentials = { ... event };
+  const credentials = { ...event };
+  console.log(credentials)
   const provider = new DefenderRelayProvider(credentials);
+  console.log(provider)
   const signer = new DefenderRelaySigner(credentials, provider, { speed: 'fast' });
+  console.log(signer)
   const forwarder = new ethers.Contract(ForwarderAddress, ForwarderAbi, signer);
-  
+
   // Relay transaction!
   const tx = await relay(forwarder, request, signature);
   console.log(`Sent meta-tx: ${tx.hash}`);
--- 01-defender-meta-txs/hardhat.config.js
+++ 01-defender-meta-txs/hardhat.config.js
@@ -15,14 +15,36 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.0",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0"
+      },
+      {
+        version: "0.7.4"
+      }
+    ]
+  },
+  defaultNetwork: "rinkeby",
   networks: {
     local: {
       url: 'http://localhost:8545'
     },
-    xdai: {
-      url: 'https://dai.poa.network',
+    // xdai: {
+    //   url: 'https://dai.poa.network',
+    //   accounts: [process.env.PRIVATE_KEY],
+    // },
+    rinkeby: {
+      url: "[ENTER API URL]",
       accounts: [process.env.PRIVATE_KEY],
+      gas: 2100000,
+      gasPrice: 8000000000
+    },
+    mumbai: {
+      url: "",
+      accounts: [process.env.PRIVATE_KEY],
+      gas: 2100000,
+      gasPrice: 8000000000
     }
   }
 };
